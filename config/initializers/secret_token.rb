c7b4206ce69c97d6d95cc55398172c7b5986514a
David Moore <math.banjo@gmail.com>
2013-03-09 20:52:06 -0800

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PictureBook::Application.config.secret_token = 'c002d2e6ff69e9137988babb0753c626d589d927d58350d652759678ac9bb0367f7170ecca961bfb4192d812000102ce82fa06c493c7423e8d83d80d31d766d4'
