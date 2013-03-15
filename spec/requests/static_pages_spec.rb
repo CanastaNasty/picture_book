a1e63dfe8969a81c6eced64c913a154ba9663e86
David Moore <math.banjo@gmail.com>
2013-03-14 19:20:54 -0700

    Update tests

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,27 +1,48 @@
 require 'spec_helper'
 
-describe "StaticPages" do
-  
-  subject { page }
+describe "static pages" do
+
+subject { page }
+
+  shared_examples_for "all static pages" do
+    it { should have_selector('h1',    text: heading) }
+    it { should have_selector('title', text: full_title(page_title)) }
+  end
 
   describe "Home page" do
-    before { visit root_path } 
+    before { visit root_path }
+    let(:heading)    { 'Picture Book' }
+    let(:page_title) { '' }
 
-    it { should have_selector('h1',    text: 'Picture Book') }
-    it { should have_selector('title', text: full_title('')) }
-  end 
+    it_should_behave_like "all static pages"
+  end
 
   describe "About Page" do
     before { visit about_path}
+    let(:heading)    { 'About' }
+    let(:page_title) { 'About' }
 
-  	it { should have_selector('h1',    text: 'About') }
-    it { should have_selector('title', text: full_title('About')) }
+    it_should_behave_like "all static pages"
   end
 
-  describe "Contact page" do
+  describe "Contact Page" do
     before { visit contact_path}
-    
-    it { should have_selector('h1',    text: 'Contact') }
-    it { should have_selector('title', text: full_title('Contact')) }
+    let(:heading)    { 'Contact' }
+    let(:page_title) { 'Contact' }
+
+    it_should_behave_like "all static pages"
+  end
+
+  it "should have the right links on the layout" do
+    visit root_path
+    click_link "About"
+    page.should have_selector 'title', text: full_title('About')
+    click_link "Contact"
+    page.should have_selector 'title', text: full_title('Contact')
+    click_link "Home"
+    click_link "Sign up now!"
+    page.should have_selector 'title', text: full_title('Sign up')
+    click_link "Picture Book"
+    page.should have_selector 'title', text: full_title('')
   end
 end
