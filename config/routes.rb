6881b92ec6c8c1b69a09339b04e0ac1454249c2d
David Moore <math.banjo@gmail.com>
2013-03-31 21:12:51 -0700

    Finish sign in

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 PictureBook::Application.routes.draw do
   resources :users
+  resources :sessions, only: [:new, :create, :destroy]
 
   root to: 'static_pages#home'
 
   match '/signup',  to: 'users#new'
-
+  match '/signin',  to: 'sessions#new'
+  match '/signout', to: 'sessions#destroy', via: :delete
   match '/about',   to: 'static_pages#about'
   match '/contact', to: 'static_pages#contact'
 
--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -39,6 +39,15 @@ describe "User pages" do
         fill_in "Confirmation", with: "foobar"
       end
 
+      describe "after saving the user" do
+        before { click_button submit }
+        let(:user) { User.find_by_email('user@example.com') }
+
+        it { should have_selector('title', text: user.name) }
+        it { should have_selector('div.alert.alert-success', text: 'Welcome') }
+        it { should have_link('Sign out') }
+      end
+
       it "should create a user" do
         expect { click_button submit }.to change(User, :count).by(1)
       end
