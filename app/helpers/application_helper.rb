7b0b6453c1a6165a50d879a54231a4fb74addc3d
David Moore <math.banjo@gmail.com>
2013-06-16 11:35:58 -0400

    safe image_tag

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -9,4 +9,16 @@ module ApplicationHelper
       "#{base_title} | #{page_title}"
     end
   end
+
+  # Image & Audio tags that can accept null
+  def safe_image_tag(source, options = {})
+    source ||= "/assets/picture_book.png"
+    image_tag(source, options)
+  end
+
+  def safe_audio_tag(source, options = {})
+  	if source != nil
+  	  audio_tag(source, options)
+  	end
+  end
 end
